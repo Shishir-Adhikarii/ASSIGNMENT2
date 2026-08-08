1711a112e4402b77b1046f0f21878f2a775c5d07
Shishir Adhikari <180571972+Shishir-Adhikarii@users.noreply.github.com>
2026-08-07 20:31:54 -0400

    Configure MongoDB and create database models

---
+++ models/Assignment.js
@@ -0,0 +1,51 @@
+const mongoose = require('mongoose');
+
+const assignmentSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+        trim: true
+    },
+
+    course: {
+        type: String,
+        required: true,
+        trim: true
+    },
+
+    description: {
+        type: String,
+        required: true,
+        trim: true
+    },
+
+    dueDate: {
+        type: Date,
+        required: true
+    },
+
+    priority: {
+        type: String,
+        enum: ['Low', 'Medium', 'High'],
+        default: 'Medium'
+    },
+
+    status: {
+        type: String,
+        enum: ['Not Started', 'In Progress', 'Completed'],
+        default: 'Not Started'
+    },
+
+    createdBy: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = mongoose.model('Assignment', assignmentSchema);
